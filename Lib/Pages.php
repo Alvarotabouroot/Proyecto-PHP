02911e78bf551dc3259f7142fffed55a931da07d
Alvaro <taboure12@gmail.com>
2023-05-31 10:00:28 +0200

    modified:   Lib/Pages.php

--- Lib/Pages.php
+++ Lib/Pages.php
@@ -8,8 +8,7 @@ class Pages{
                 $$name = $value;
             }
         }
-        // require_once "views/layout/header.php";
+
         require_once "../views/$pageName.php";
-        // require_once "views/layout/footer.php"; 
     }
 }
